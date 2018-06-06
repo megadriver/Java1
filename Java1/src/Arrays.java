3a15230d1c02c6f0bd633278d47b8169d52b4780
Lenovo-T440p <Lenovo-T440p@DESKTOP-K3EOI7B>
2018-06-06 21:37:11 +0200

    HW2

--- Java1/src/Arrays.java
+++ Java1/src/Arrays.java
@@ -4,6 +4,7 @@ public class Arrays {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("HelloWorld!");
+		System.out.println("HelloWorld2!");
 	}
 
 }
